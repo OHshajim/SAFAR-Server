9583bdf5299df1ce5feaa938c522dc9a3e2f3142
OHshajim <ajshajimmax@gmail.com>
2024-04-26 07:15:08 +0600

    data post

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const express = require("express")
-const cors = require("cors")
-require('dotenv').config()
-const app = express()
+const express = require("express");
+const cors = require("cors");
+require('dotenv').config();
+const app = express();
 const port = process.env.PORT || 5000;
 
 
 // middleware 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.q9eobgc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -27,13 +27,26 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
-        
+        const spotsCollection = client.db("SpotsDB").collection("spots");
+        // app.get("/spots", async (req, res) => {
+        //     const cursor = spotsCollection.find()
+        //     const result = await cursor.toArray()
+        //     res.send(result)
+        // })
+
+        app.post("/spots", async (req, res) => {
+            const spot = req.body;
+            console.log(spot);
+            const result = await spotsCollection.insertOne(spot)
+            res.send(result);
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
@@ -44,5 +57,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Tourism server listening on port ${port}`)
 })
