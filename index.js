9919e517c3dd3cb5980012e5cdac4cb5d19c5cef
OHshajim <ajshajimmax@gmail.com>
2024-04-29 12:43:08 +0600

    deply server side

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
         const database = client.db("SpotsDB");
         const spotsCollection = database.collection("spots");
         const countriesCollection = database.collection("countries");
